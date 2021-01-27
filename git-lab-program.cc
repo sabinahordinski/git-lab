c19f6405a0e9cd9ebc5526ea15b990843769c004
Sabina Hordinski <sh070319@ohio.edu>
2021-01-27 18:10:13 -0400

    git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sabina Hordinski
+ *        Date: 27 January 2021
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
